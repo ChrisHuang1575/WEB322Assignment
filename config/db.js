876bf86cc1fa9f4bf8c99e1542344b34e544dc53
YongAn Huang <yhuang291@myseneca.ca>
2022-12-09 22:30:38 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,8 @@
+// Defines which database will be used
+
+// if (process.env.NODE_ENV == "production") {
+    module.exports = {mongoURI: "mongodb+srv://dbUser:ChrisH1997@senecaweb.c1ps4uo.mongodb.net/web322_A4?retryWrites=true&w=majority"}
+    // module.exports = {mongoURI: "mongodb+srv://root:rootadmin@cluster0-ehmnn.mongodb.net/test?retryWrites=true&w=majority"}
+// } else {
+//     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+// }
